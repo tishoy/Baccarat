4daf27ae553e8cf1ad69a54be3840bb98528b8da
tishoy <hantishoy@sina.com>
2016-03-19 19:09:36 +0800

    add Pair Win and Tie Win, Card number 52 * 8

--- main.go
+++ main.go
@@ -4,60 +4,76 @@ import (
 "fmt"
 "time"
 "math/rand"
+"strconv"
 )
 
-var resultCard []int
+type card struct {
+	point string
+	color, value int
+}
+
+var resultCard []*card
 
 func main() {
-	cardList := []int{}
-	for i := 1; i <= 52; i++ {
-		cardList = append(cardList, i)
-		// fmt.Print(cardList[i])
+	cardList := []*card{}
+	for i := 1; i <= 52 * 8; i++ {
+		card := generateCard(i)
+		cardList = append(cardList, card)
 	}
 	
 	for i := 0; i < 4; i++ {
 		if i == 0 {
-			fmt.Print("zhuang")
+			fmt.Print("Bank get card")
 		} else if i == 2 {
-			fmt.Print("xian")
+			fmt.Print(" ===== Play get card")
 		}
 		rand.Seed(time.Now().UnixNano())
-		card := rand.Intn(52 - i)
-    	cardList = append(cardList[:card],cardList[card+1:]...)
-		outPutCard(3 + card % 4, 1 + card / 4)
+		index := rand.Intn(52 * 8 - i)
+		fmt.Printf("%c", cardList[index].color)
+		fmt.Print(cardList[index].point)
+    	cardList = append(cardList[:index], cardList[index+1:]...)
+    	resultCard = append(resultCard, cardList[index])
 	}
 
 	checkWin()
 }
 
-func outPutCard(color int, num int) {
-	fmt.Printf("%c", color)
+func generateCard(i int) (*card) {
+	num := i / 32 + 1
+	color := i % 4 + 3
+	value := 0
+	point := ""
 	switch num {
 		case 1:
-			fmt.Print("A")
-			resultCard = append(resultCard, 1)
+			value = 1
+			point = "A"
 		case 11:
-			fmt.Print("J")
-			resultCard = append(resultCard, 1)
+			value = 1
+			point = "J"
 		case 12:
-			fmt.Print("Q")
-			resultCard = append(resultCard, 1)
+			value = 1
+			point = "Q"
 		case 13:
-			fmt.Print("K")
-			resultCard = append(resultCard, 1)
+			value = 1
+			point = "K"
 		default:
-			fmt.Print(num)
-			resultCard = append(resultCard, num)
+			value = num
+			point = strconv.Itoa(num)
 	}
+	return &card{point: point, color: color, value: value}
 }
 
 func checkWin() {
-	zhuangResult := (resultCard[0] + resultCard[1]) % 10
-	xianResult := (resultCard[2] + resultCard[3]) % 10
+	if (resultCard[0].point == resultCard[1].point || resultCard[2].point == resultCard[3].point) {
+		fmt.Print("\n\rPair Win")
+		return
+	}
+	zhuangResult := (resultCard[0].value + resultCard[1].value) % 10
+	xianResult := (resultCard[2].value + resultCard[3].value) % 10
 	if zhuangResult > xianResult {
-		fmt.Print("\n\rzhuang Win")
+		fmt.Print("\n\rBank Win")
 	} else if zhuangResult < xianResult {
-		fmt.Print("\n\rxian Win")
+		fmt.Print("\n\rPlay Win")
 	} else if zhuangResult == xianResult {
 		fmt.Print("\n\rhe Win")
 	}
